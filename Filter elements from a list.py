e6fce3d5ba902e9ccf5f55b55ade5662fb862ca6
Sokly Hour <hoursokly011@gmail.com>
2024-09-24 21:55:28 -0500

    add

---
+++ Filter elements from a list.py
@@ -0,0 +1,7 @@
+#  based on a condition
+numbers = [1, 2, 3, 4, 5, 6]
+
+even_numbers = [num for num in numbers if num % 2 == 0]  # List comprehension for even numbers
+
+print(f"Even numbers: {even_numbers}")
+
