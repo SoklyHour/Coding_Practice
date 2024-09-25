fee57341f19be0c3c225918c112ba4fc1448a524
Sokly Hour <hoursokly011@gmail.com>
2024-09-24 22:01:28 -0500

    add

---
+++ create a simple function that takes arguments and performs calculations.py
@@ -0,0 +1,13 @@
+def calculate_area(length, width):
+    
+  """Calculates the area of a rectangle."""
+
+  return length * width
+
+
+# Example usage
+
+area = calculate_area(5, 3)
+
+print(f"Area of rectangle: {area}")
+
