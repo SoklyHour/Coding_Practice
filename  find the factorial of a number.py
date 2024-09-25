35fa1f80d567e502a18a47ea3baefc930e13cbca
Sokly Hour <hoursokly011@gmail.com>
2024-09-24 21:07:30 -0500

    add

---
+++  find the factorial of a number.py
@@ -0,0 +1,11 @@
+# Write a Python program to find the factorial of a number.
+
+def factorial(n):
+    if n == 0:  
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+number = 5
+result =factorial(number)
+print(f"the factorial of {number} is {result}")
