80fdca0991a4143a23bc031a37cfc7a7c336f9eb
Sokly Hour <hoursokly011@gmail.com>
2024-09-24 21:16:28 -0500

    add

---  find the largest element in a list.py
+++  find the largest element in a list.py
@@ -11,3 +11,17 @@ nums = [10, 20, 1]
 largest_numbers = find_largest(nums)
 
 print(f"The largest number is {largest_numbers}")
+
+# Write a Python program to find the smallest element in a list.
+
+def find_smallest(numbers):
+    smallest = numbers[0]
+
+    for num in numbers:
+        if num < smallest:
+            num = smallest
+    return smallest
+    
+num = [1,2,3,4]
+smallest_numbers = find_smallest(num)
+print(f"{smallest_numbers}")
