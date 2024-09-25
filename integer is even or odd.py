de6055098833b25f64015b8d27bdb5987acfdf95
Sokly Hour <hoursokly011@gmail.com>
2024-09-24 20:51:32 -0500

    add

---
+++ integer is even or odd.py
@@ -0,0 +1,11 @@
+# How do you check if an integer is even or odd?
+# You can check if an integer is even by dividing it by 2 and checking the remainder.
+def is_even(number):
+    return number % 2 == 0
+    
+
+print(is_even(4))   # Output: True
+print(is_even(7))   # Output: False
+print(is_even(0))   # Output: True
+print(is_even(-2))  # Output: True
+print(is_even(-3))  # Output: False
