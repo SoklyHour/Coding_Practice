ebc31382b0bdd096bd1bbd13f9d0bfc3ba15c850
Sokly Hour <hoursokly011@gmail.com>
2024-09-24 20:48:22 -0500

    add

---
+++ calculate the sum of two integers.py
@@ -0,0 +1,15 @@
+# How do you calculate the sum of two integers?
+# To calculate the sum of two integers, you can simply add them.
+
+def sum_of_integers(a, b):  
+    return a+b
+
+
+result = sum_of_integers(3, 5)  # Example with integers
+print(result)  # Output: 8
+
+result = sum_of_integers(-2, 4)  # Example with a negative integer
+print(result)  # Output: 2
+
+result = sum_of_integers(10, 15)  # Example with larger integers
+print(result)  # Output: 25
